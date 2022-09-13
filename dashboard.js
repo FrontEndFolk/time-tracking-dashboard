51762bf8b47290c24b6c6074c9f32cac1d9864f1
The-beginner-2 <vovaulchenko@gmail.com>
2022-09-13 10:49:19 +0300

    js done but not polished yet css not exist

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,16 @@
 const jsonURL = 'data.json';
+const selectors = document.querySelectorAll('.dashboard__selectors');
 
 function getJSON(url) {
     return fetch(url).then(response => response.json());
 }
 
 class dashBoardItem {
+    static PERIODS = {
+        daily: 'day',
+        weekly: 'week',
+        monthly: 'month',
+    }
     constructor(data, container = '.dashboard__body', view = 'daily') {
         this.data = data;
         this.container = document.querySelector(container);
@@ -16,14 +22,28 @@ class dashBoardItem {
     createMarkUp() {
         const { title, timeframes } = this.data;
         const { current, previous } = timeframes[this.view];
+        const id = title.toLowerCase().replace(' ', '-');
         this.container.insertAdjacentHTML('beforeend', `
-        <div class='dashboard__item'>
+        <div class='dashboard__item dashboard__item--${id}'>
         <p class='dashboard__title'>${title}</p>
         <p class='dashboard__cur'>${current}hrs</p>
-        <p class='dashboard__prev'>last week ${previous}hrs</p>
+        <p class='dashboard__prev'>last ${this.view} ${previous}hrs</p>
         </div>
         `);
 
+        this.curr = document.querySelector(`.dashboard__item--${id} .dashboard__cur`);
+        this.prev = document.querySelector(`.dashboard__item--${id} .dashboard__prev`);
+
+    }
+
+
+
+    changeView(view) {
+        view = view.trim().toLowerCase();
+        const { current, previous } = this.data.timeframes[view];
+        this.view = view;
+        this.curr.textContent = `${current} hrs`;
+        this.prev.textContent = `last ${this.view} ${previous}hrs`
     }
 
 }
@@ -32,6 +52,10 @@ class dashBoardItem {
 document.addEventListener('DOMContentLoaded', () => {
     getJSON(jsonURL).then(data => {
         const activities = data.map(el => new dashBoardItem(el));
+        selectors.forEach(e => e.addEventListener('click', (event) => {
+            activities.forEach(act => act.changeView(event.target.textContent));
+        }));
     })
+
 })
 
